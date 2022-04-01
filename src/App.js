43ba7cc338811ce93018584591b4436639b92732
Ayokunle Ogunfidodo <ayokunleogunfidodo@gmail.com>
2022-04-01 22:02:34 +0300

    feedback-app

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import {v4 as uuidv4} from 'uuid'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { useState } from 'react';
+import Header from './components/Header'
+import FeedbackList from './components/FeedbackList';
+import FeedbackData from './data/FeedbackData';
+import FeedbackStats from './components/FeedbackStats';
+import FeedbackForm from './components/FeedbackForm';
+import AboutIconLink from './components/AboutIconLink';
+import AboutPage from './pages/AboutPage';
+import { FeedbackProvider } from './context/FeedbackContext';
+import Post from './components/Post';
+
+
+function App() {
+  
+  const [feedback, setFeedback]= useState(FeedbackData)
+
+  const addFeedback = (newFeedback)=>{
+    newFeedback.id = uuidv4()
+      setFeedback([newFeedback, ...feedback])
+  }
+
+  const deleteFeedback =(id)=>{
+    if(window.confirm('Are you sure you want to delete?')){
+      setFeedback(feedback.filter((item)=> item.id !== id))
+    }
+
+  }
+  return (
+    <FeedbackProvider>
+    <Router>
+    <Header />
+    <div className="container">
+      <Routes>
+    <Route exact path='/' element={
+      <>
+      <FeedbackForm handleAdd={addFeedback} />
+     <FeedbackStats feedback={feedback} />
+     <FeedbackList feedback={feedback}  handleDelete= {deleteFeedback}/>
+      </>
+    }>
+     
+     </Route> 
+     <Route path='/about' element={<AboutPage />} />
+     <Route path='/post/*' element={<Post />} />
+     </Routes>
+     <AboutIconLink/>
+   </div>
+   </Router>
+   </FeedbackProvider>
+  );
+}
+
+export default App;
---
+++ src/data/FeedbackData.js
@@ -0,0 +1,18 @@
+const FeedbackData = [{
+    id:1,
+    rating:10,
+    text: "lorem ipsum CouchDB is an ECMAScript language with a project or graphics within a package manager with Node. Chai is a package manager with text, arrays, dates. Object Model DOM is a library that gets called observers, and manipulate to load JavaScript engine. IIFE is a software development. Mocha is a framework based on data fetching."
+  },
+  {
+    id:2,
+    rating:9,
+    text: "World Wide Web form to the result of their design pattern in JavaScript. Grunt is a creational design pattern in 2D or included from Node. Moment. Underscore is a child function has been updated in a task runner aiming at explaining the ECMAScript 3. Currying is supported by all methods and libraries developed and Node."
+  },
+  {
+    id:3,
+    rating:8.5,
+    text: "JSON-LD is a standard libraries, the majority of an optimization used for Node. Mediator Pattern is a general reusable solution to its dependents, called the client side, JavaScript engine. OO framework, extensive Ajax programming styles when its own build system and language-neutral interface that the client functionality for example games, and Cordova. JSLint is a clean underlying data fetching. Loading new objects."
+  }
+  ]
+
+  export default FeedbackData
